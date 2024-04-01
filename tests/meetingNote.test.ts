656c033db0475bbdeda351ed8ca15284153b476a
simone-viozzi <viozzis333@gmail.com>
2024-04-01 11:16:09 +0200

    add tests. move functions that not use silverbullet into utils

--- tests/meetingNote.test.ts
+++ tests/meetingNote.test.ts
@@ -1,44 +1,44 @@
 import { assertEquals } from "https://deno.land/std/assert/assert_equals.ts";
-import { sanitizeTitle, parseDateWithFormats } from "../meetingNote.ts";
+import { sanitizeTitle, parseDateWithFormats } from "../utils.ts";
 // Assuming `dayjs` or similar date utility is used and imported correctly
 
 
 Deno.test("sanitizeTitle with special characters and consecutive hyphens", () => {
-  assertEquals(sanitizeTitle("--STM++meeting1=="), "STM - meeting1");
+  assertEquals(sanitizeTitle("--[tag]++meeting1=="), "tag - meeting1");
 });
 
 Deno.test("sanitizeTitle with spaces", () => {
-  assertEquals(sanitizeTitle("STM meeting1"), "STM meeting1");
+  assertEquals(sanitizeTitle("tag  meeting1"), "tag meeting1");
 });
 
 Deno.test("sanitizeTitle with a single special character", () => {
-  assertEquals(sanitizeTitle("STM|meeting1"), "STM - meeting1");
+  assertEquals(sanitizeTitle("[tag]|meeting1"), "tag - meeting1");
 });
 
 Deno.test("sanitizeTitle with multiple spaces", () => {
-  assertEquals(sanitizeTitle("STM    meeting1"), "STM meeting1");
+  assertEquals(sanitizeTitle("[tag]    meeting1"), "tag - meeting1");
 });
 
 Deno.test("sanitizeTitle with hyphens without spaces", () => {
-  assertEquals(sanitizeTitle("STM-meeting1"), "STM - meeting1");
+  assertEquals(sanitizeTitle("tag-meeting1"), "tag - meeting1");
 });
 
 Deno.test("sanitizeTitle with colon", () => {
-  assertEquals(sanitizeTitle("STM:meeting1"), "STM - meeting1");
+  assertEquals(sanitizeTitle("[tag]:meeting1"), "tag - meeting1");
 });
 
 Deno.test("sanitizeTitle with leading and trailing spaces", () => {
-  assertEquals(sanitizeTitle("  STM meeting1  "), "STM meeting1");
+  assertEquals(sanitizeTitle("  [tag] meeting1  "), "tag meeting1");
 });
 
 Deno.test("sanitizeTitle with @ symbol", () => {
-  assertEquals(sanitizeTitle("STM@meeting1"), "STM - meeting1");
+  assertEquals(sanitizeTitle("[tag]@meeting1"), "tag - meeting1");
 });
 
 Deno.test("sanitizeTitle with # symbol", () => {
-  assertEquals(sanitizeTitle("STM#meeting1"), "STM - meeting1");
+  assertEquals(sanitizeTitle("[tag]#meeting1"), "tag - meeting1");
 });
 
 Deno.test("sanitizeTitle with & symbol", () => {
-  assertEquals(sanitizeTitle("STM&meeting1"), "STM - meeting1");
+  assertEquals(sanitizeTitle("[tag]&meeting1"), "tag - meeting1");
 });
